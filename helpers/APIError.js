6390d3e32f1c5cdefec8b4f8cef7bd9290c5ded3
youness <youness.hakky@gmail.com>
2017-12-29 22:31:04 +0000

    more cleaning & structuring

--- helpers/APIError.js
+++ helpers/APIError.js
@@ -5,7 +5,7 @@ class ExpendableError extends Error {
     super(message, status)
     this.message = message;
     this.code = httpStatus[status];
-    this.status = status || 500
+    this.status = status || 500;
   }
 }
 
@@ -16,6 +16,4 @@ class APIError extends ExpendableError {
   }
 }
 
-const apiError = new APIError('something went wrong')
-
-console.log(apiError.status)
+module.exports = APIError;
